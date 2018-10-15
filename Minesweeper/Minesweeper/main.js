f828a8ad994a9e78f42643bb08635ef5edcff629
Norbert <hollowjokken@gmail.com>
2018-10-15 13:42:41 +0300

    added difficulty for the game

--- Minesweeper/Minesweeper/main.js
+++ Minesweeper/Minesweeper/main.js
@@ -1,11 +1,21 @@
+minesNumber = {
+    '9': 10,
+    '16': 40,
+    '32': 99
+}
+let tableCells = 9;
+let tableRows = 9;
+
 let tableMines;
 let tableValues;
 let tableData;
 
-function generateMines() {
-    let randomMines = []
-    while (randomMines.length < 8) {
-        let randomnumber = Math.floor(Math.random() * 63);
+
+function generateMines(difficulty) {
+    let randomMines = [];
+    const mines = minesNumber[`${difficulty}`];
+    while (randomMines.length < mines) {
+        let randomnumber = Math.floor(Math.random() * (tableCells * tableRows));
         if (randomMines.indexOf(randomnumber) > -1) continue;
         randomMines[randomMines.length] = randomnumber;
     }
@@ -15,12 +25,11 @@ function generateMines() {
 function generateTableValues(mines) {
     let allData = [];
     let id = 0;
-    for (let j = 0; j <= 9; j++) {
+
+    for (let j = 0; j <= (tableRows + 1); j++) {
         allData.push([]);
-        for (let i = 0; i <= 9; i++) {
-
-            if (i !== 0 && j !== 0 && i !== 9 && j !== 9) {
-
+        for (let i = 0; i <= (tableCells + 1); i++) {
+            if (i !== 0 && j !== 0 && i !== (tableCells + 1) && j !== (tableRows + 1)) {
                 if (jQuery.inArray(id, mines) != -1) {
                     allData[j][i] = {
                         id,
@@ -35,7 +44,6 @@ function generateTableValues(mines) {
                     }
                 }
                 id++;
-
             } else {
                 allData[j][i] = {
                     value: 'M'
@@ -50,8 +58,8 @@ function generateFullTabelData(table) {
     let finalData = table;
     let bombNr;
 
-    for (let i = 1; i <= 8; i++) {
-        for (let j = 1; j <= 8; j++) {
+    for (let i = 1; i <= tableRows; i++) {
+        for (let j = 1; j <= tableCells; j++) {
             bombNr = 0;
             if (table[i][j].value !== 'B') {
                 if (table[i][j - 1].value === 'B') bombNr++;
@@ -72,11 +80,9 @@ function generateFullTabelData(table) {
 function createMinesweeperTable() {
     let id = 0;
     let mainTabel = `<table id="main-board-table">`;
-    for (let j = 1; j <= 8; j++) {
+    for (let j = 1; j <= tableRows; j++) {
         mainTabel += `<tr id=tr${j}>`;
-        for (let i = 1; i <= 8; i++) {
-            // mainTabel += `<td id=${id}>${tableData[j][i].value}</td>`;
-            // mainTabel += `<td id=${id}>${tableData[id].value}</td>`;
+        for (let i = 1; i <= tableCells; i++) {
             mainTabel += `<td id=${id}></td>`;
             id++;
         }
@@ -124,11 +130,10 @@ function showZone(location) {
     return;
 }
 
-function winner(stage) {
+function winner() {
     let cell;
-    for (let i = 0; i < stage; i++) {
+    for (let i = 0; i < (tableRows * tableCells); i++) {
         cell = findElem(i);
-        console.log('TCL: winner -> cell', cell);
         if (cell.value !== 'B' && cell.hidden === true) {
             return false;
         }
@@ -136,36 +141,51 @@ function winner(stage) {
     return true;
 }
 
-function restartGame() {
+function revealAllMines(mines) {
+    mines.forEach(mineId => {
+        const mine = findElem(mineId)
+        $(`#${mine.id}`).text(mine.value).css("background-color", "red");
+    })
+}
+
+function restartGame(difficulty) {
+    tableCells = difficulty;
+    tableRows = difficulty === 32 ? 16 : difficulty;
+
     $('#main-board-table').remove();
-    tableMines = generateMines();
+    $('#minesweeper-table').append(createMinesweeperTable());
+
+    tableMines = generateMines(difficulty);
     tableValues = generateTableValues(tableMines);
     tableData = generateFullTabelData(tableValues);
 
-    $('#minesweeper-table').append(createMinesweeperTable());
-}
-$('#resetBtn').click(() => { location.reload() });
+    $('#main-board-table').click((e) => {
+        let target = e.target;
+        let chosen = findElem(parseInt(target.id));
 
-restartGame();
-
-$('#main-board-table').click((e) => {
-    let target = e.target;
-    let chosen = findElem(parseInt(target.id));
-
-    if ($(target).is('td')) {
-        if (chosen.value === 'B') {
-            $(target).css("background-color", "red");
-            alert('LOSE!!!')
-                // restartGame();
-            location.reload();
-        } else {
-            showZone(chosen);
+        if ($(target).is('td')) {
+            if (chosen.value === 'B') {
+                $(target).css("background-color", "red");
+                revealAllMines(tableMines);
+                $('#main-board-table').css({ "pointer-events": "none" });
+                setTimeout(() => { alert('LOSE!!!') });
+            } else {
+                showZone(chosen);
+            }
         }
-    }
 
-    if (winner(8 * 8)) {
-        alert('Winner!!!');
-        location.reload();
-    }
+        if (winner()) {
+            $('#main-board-table').css({ "pointer-events": "none" });
+            setTimeout(() => { alert('Winner!!!') });
+        }
 
-})
+    })
+
+}
+
+$('#resetBtn').click(() => {
+    const difficulty = parseInt($("#game-level").val());
+    restartGame(difficulty);
+});
+
+restartGame(parseInt($("#game-level").val()));
